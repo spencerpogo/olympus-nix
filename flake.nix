d06709b37c2e139c04d8968d7ebb6548c2b49efe
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-06-11 17:08:30 -0700

    Use nixpkgs correctly

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.olympus = nixpkgs.callPackage ./olympus.nix {};
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.x86_64-linux.olympus = pkgs.callPackage ./olympus.nix {};
   };
 }
