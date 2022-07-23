8507aee17aa2b67670ad491e6448703d520a47fe
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-07-23 12:39:04 -0700

    Remove unnecessary input

--- nfd.nix
+++ nfd.nix
@@ -21,7 +21,6 @@ lua51Packages.buildLuarocksPackage {
   rockspecDir = "lua";
 
   extraVariables.LUA_LIBDIR = "${lua5_1}/lib";
-  nativeBuildInputs = [ pkg-config ];
 
   fixupPhase = ''
     find $out -name nfd_zenity.so -execdir mv {} nfd.so \;
